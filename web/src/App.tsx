860c08b4b60eb76500df53a3b8170f8af51b5d5f
João Vitor Matos <138345613+JoaoVitor-Matos@users.noreply.github.com>
2025-05-08 00:03:32 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InvitedPage from './pages/invited';
 import AcceptedPage from './pages/accepted';
 import NewLeadPage from './pages/new';
 import DeclinedPage from './pages/declined';
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-    <Route path="/invited" element={<InvitedPage />} />
-    <Route path="/accepted" element={<AcceptedPage />} />
-    <Route path="/declined" element={<DeclinedPage />} />
-    <Route path="/new" element={<NewLeadPage />} />
-  </Routes>
+        <Route path="/" element={<InvitedPage />} /> 
+        <Route path="/invited" element={<InvitedPage />} />
+        <Route path="/accepted" element={<AcceptedPage />} />
+        <Route path="/declined" element={<DeclinedPage />} />
+        <Route path="/new" element={<NewLeadPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
